a3e20534aacb86c5b0930b4f5e55f7a2273b1b38
Albert <albertjohan2707@gmail.com>
2024-01-28 23:41:04 -0400

    first commit

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -157,10 +157,35 @@ const Projects = () => {
         </motion.div>
         <motion.div
           className="item"
+          initial="rest"
+          whileHover="hover"
+          animate="rest"
           style={{ scale: scaleProgress3, opacity: opacityProgress3 }}
           ref={socialRef}
         >
           <img src="/images/social.png" />
+          <motion.div variants={infoMotion} className="hover-info">
+            <motion.button
+              variants={infoMotion}
+              whileTap={{ scale: 0.9 }}
+              transition={{ type: "spring", duration: 0.2 }}
+            >
+              View on GitHub
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                />
+              </svg>
+            </motion.button>
+          </motion.div>
         </motion.div>
       </div>
     </div>
