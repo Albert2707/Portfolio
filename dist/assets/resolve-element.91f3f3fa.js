47e359c2be1bd1790fe87556c4f1fdd4fad3e407
Albert <albertjohan2707@gmail.com>
2025-02-13 13:03:10 -0400

    feat: add new images and improve styling for responsiveness

---
+++ dist/assets/resolve-element.91f3f3fa.js
@@ -0,0 +1,1 @@
+import{h as a}from"./motion.aea366d8.js";function v(r,o,i){var f;if(typeof r=="string"){let u=document;o&&(a(Boolean(o.current)),u=o.current),i?((f=i[r])!==null&&f!==void 0||(i[r]=u.querySelectorAll(r)),r=i[r]):r=u.querySelectorAll(r)}else r instanceof Element&&(r=[r]);return Array.from(r||[])}export{v as r};
